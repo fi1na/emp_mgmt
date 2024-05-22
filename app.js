3f309a1fd66343a025a38bb0e098fe2171154ef1
fi1na <fionadash.11@gmail.com>
2024-05-22 22:18:41 +0530

    git add3

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 function signUp(){
-    
+    var fullname=document.getElementById("fullname").value;
+    var phonenumber=document.getElementById("phonenumber").value;
+    var email=document.getElementById("email").value;
+    var password=document.getElementById("password").value;
+
+ var userObj={
+    fullname,
+    phonenumber,
+    email,
+    password,
+ }
+ console.log(userObj)
 }
